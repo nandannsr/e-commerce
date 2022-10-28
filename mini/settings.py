c94b3aa3f4482bbe3821235bdb3eef6cd00d7a66
nandannsr <nandannsrkumar@gmail.com>
2022-10-28 14:19:48 +0530

    Allowed Host

--- mini/settings.py
+++ mini/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["django-env.eba-aw2d3tmt.us-west-2.elasticbeanstalk.com"]
 
 
 # Application definition
